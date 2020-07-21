a35c89dc46217ae36ff6d4259f6ba08afd3b8e05
amrutamsunil <amrutamsunil4@gmail.com>
2020-07-21 13:28:50 +0530

    Promises part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { MatSelectModule } from "@angular/material/select";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
-
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import "hammerjs";
 
 import { AppComponent } from "./app.component";
@@ -63,6 +63,7 @@ import { LoginComponent } from "./login/login.component";
     ReactiveFormsModule,
     MatSelectModule,
     MatSlideToggleModule,
+    MatProgressSpinnerModule,
   ],
   entryComponents: [LoginComponent],
   providers: [DishService, PromotionService, LeaderService],
